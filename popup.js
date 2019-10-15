315d8448f220e3add15e55df3f7f0382477e54d6
Caleb Kaiser <babyboi@Calebs-MacBook-Pro.local>
2019-10-15 09:22:19 -0400

    Add conditional coloring

--- popup.js
+++ popup.js
@@ -4,14 +4,27 @@ chrome.tabs.executeScript({
     let req = new XMLHttpRequest();
     req.open('POST', 'http://a1a48892be7c911e9b3960a515995724-1930931035.us-west-2.elb.amazonaws.com/sentiment/classifier');
     req.setRequestHeader('Content-Type', 'application/json');
+
     req.onload = function() {
       let response = JSON.parse(req.responseText);
-      document.getElementById('output').innerHTML = response;
+      let elem = document.getElementById('output');
+
+      if (response == 'positive') {
+        elem.cssText = 'color: blue';
+      } else if (response == 'negative') {
+        elem.cssText = 'color: red';
+      } else {
+        elem.cssText = 'color: green';
+        response = 'unclear'
+      }
+      elem.innerHTML = response;
     }
+
     let sendData = JSON.stringify(
         {
           "review": selection[0]
         }
     );
+    
     req.send(sendData);
 });
